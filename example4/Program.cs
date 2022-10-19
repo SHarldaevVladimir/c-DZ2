7dc63244224cdc1ef6c79723c95ae98bb1b31d9a
VladimirSharldaev <vladimirsarldaev43246@gmail.com>
2022-10-20 00:01:59 +0300

    add example 4

---
+++ example4/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 11 HARD - необязательная: Напишите программу, которая принимает на вход целое число любой разрядности и
+//  удаляет вторую цифру слева направо этого числа. И, конечно же, через строку решать нельзя.
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+int onlyNumber = 0;
+int num2 = 0;
+ while (num > 99)
+{
+    onlyNumber = num % 10;
+    num2 = num2 * 10;
+    num2 = num2 + onlyNumber;
+    num = num / 10;
+
+}
+onlyNumber = num / 10;
+num2 = num2 * 10;
+num2 = num2 + onlyNumber;
+
+Console.WriteLine(num2);
+
+
+// int FindSecondNumber(int num)
+// { 
+// while (num > 99) 
+// {
+
+//     num = num/10;
+// }
+// int secondNum = num % 10;
+// return secondNum;
+// }
+// FindSecondNumber(num);
